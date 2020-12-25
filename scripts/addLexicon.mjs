3ebf3c18a7e148e6428fcb711565e793552d122b
Khyber Sen <kkysen@gmail.com>
2020-12-25 05:29:20 -0500

    Removed non a, I one-letter words from English lexicons.

--- scripts/addLexicon.mjs
+++ scripts/addLexicon.mjs
@@ -3,11 +3,16 @@ import {promises as fsp} from "fs";
 /**
  * @param {string} words
  * @return {string}
+ * @param {boolean|null|undefined|(function(string[]): string[])} and
  */
-function filterWords(words) {
-    return words
+function filterWords(words, and = undefined) {
+    let wordList = words
         .split("\n")
-        .filter(s => /^\p{Ll}+$/u.test(s))
+        .filter(s => /^\p{Ll}+$/u.test(s));
+    if (and) {
+        wordList = and(wordList);
+    }
+    return wordList
         .sort()
         .sort((a, b) => a.length - b.length)
         .join("\n");
@@ -21,7 +26,11 @@ function filterWords(words) {
 async function addLexicon(name, path) {
     console.log(`adding lexicon "${name}" from "${path}"`);
     const buffer = await fsp.readFile(path);
-    await fsp.writeFile(`./public/lexicons/${name}.txt`, filterWords(buffer.toString()));
+    const words = filterWords(buffer.toString(), /english/i.test(name) && (words => {
+        // lots of dictionaries include other single letter words for some reason
+        return ["a", "I", ...words.filter(s => s.length !== 1)];
+    }));
+    await fsp.writeFile(`./public/lexicons/${name}.txt`, words);
 }
 
 async function asyncMain() {
